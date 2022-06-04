7a86b7ac5d1a408c405d083c7b57e8c2c1177125
Nicholas Pettas <npettas@gmail.com>
2022-06-04 16:22:54 -0700

    some fixes for scripts

--- update.sh
+++ update.sh
@@ -6,23 +6,23 @@ cd ${SCRIPT_DIR}
 git fetch --all
 git reset --hard origin/master
 
-sed -i -e "s#/Users/nick/projects/superslicer-configs#${SCRIPT_DIR}#g" "$SCRIPT_DIR/printer/Standard.ini" &&
- rm "$SCRIPT_DIR/printer/Standard.ini-e"
+sed -i -e "s#/Users/nick/projects/superslicer-configs#${SCRIPT_DIR}#g" "$SCRIPT_DIR/print/Standard.ini" &&
+ rm "$SCRIPT_DIR/print/Standard.ini-e"
 
 sed -i -e "s#/Users/nick/projects/superslicer-configs#${SCRIPT_DIR}#g" "$SCRIPT_DIR/print/Flexible.ini" &&
  rm "$SCRIPT_DIR/print/Flexible.ini-e"
 
-sed -i -e "s#/Users/nick/projects/superslicer-configs#${SCRIPT_DIR}#g" "$SCRIPT_DIR/print/Standard.ini" &&
- rm "$SCRIPT_DIR/print/Standard.ini-e"
+sed -i -e "s#/Users/nick/projects/superslicer-configs#${SCRIPT_DIR}#g" "$SCRIPT_DIR/print/Slow.ini" &&
+ rm "$SCRIPT_DIR/print/Slow.ini-e"
 
 sed "s#SCRIPT_DIR=#SCRIPT_DIR=${SCRIPT_DIR}#" superslicer.sh > /Applications/SuperSlicer.app/Contents/MacOS/superslicer.sh
 sed -i -e "s#<string>parameterized-app.sh</string>#<string>superslicer.sh</string>#" \
 	/Applications/SuperSlicer.app/Contents/Info.plist &&
 	rm /Applications/SuperSlicer.app/Contents/Info.plist-e
 
-sed -i -e "s#klipper_estimator#klipper_estimator_$(uname)#" "$SCRIPT_DIR/print/Standard.ini" &&
+sed -i -e "s#scripts/klipper_estimator#scripts/klipper_estimator_$(uname)#" "$SCRIPT_DIR/print/Standard.ini" &&
   rm "$SCRIPT_DIR/print/Standard.ini-e"
-sed -i -e "s#klipper_estimator#klipper_estimator_$(uname)#" "$SCRIPT_DIR/print/Standard.ini" &&
-	rm "$SCRIPT_DIR/print/Standard.ini-e"
-sed -i -e "s#klipper_estimator#klipper_estimator_$(uname)#" "$SCRIPT_DIR/print/Standard.ini" &&
-	rm "$SCRIPT_DIR/print/Standard.ini-e"
+sed -i -e "s#scripts/klipper_estimator#scripts/klipper_estimator_$(uname)#" "$SCRIPT_DIR/print/Flexible.ini" &&
+	rm "$SCRIPT_DIR/print/Flexible.ini-e"
+sed -i -e "s#scripts/klipper_estimator#scripts/klipper_estimator_$(uname)#" "$SCRIPT_DIR/print/Slow.ini" &&
+	rm "$SCRIPT_DIR/print/Slow.ini-e"
